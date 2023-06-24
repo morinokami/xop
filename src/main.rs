72ea7a15639177856266e14331cab97a37f73bf3
Shinya Fujino <shf0811@gmail.com>
2023-06-24 22:48:41 +0900

    Add macOS support

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
 use clap::Parser;
 use std::error::Error;
-
-type MyResult<T> = Result<T, Box<dyn Error>>;
+use std::process::Command;
+use which::which;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 /// xop opens files from a shell
 pub struct Args {
     /// Files to open
-    #[arg(value_name = "FILES")]
+    #[arg(value_name = "FILES", required(true))]
     files: Vec<String>,
 }
 
-pub fn run(args: Args) -> MyResult<()> {
-    println!("{:?}", args);
+pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
+    let opener = find_opener()?;
+    for file in &args.files {
+        call_open(&opener, file)?;
+    }
     Ok(())
+}
+
+pub fn call_open(command: &str, path: &str) -> Result<(), Box<dyn Error>> {
+    Command::new(command).arg(path).spawn()?;
+    Ok(())
+}
+
+pub fn find_opener() -> Result<String, Box<dyn Error>> {
+    if cfg!(target_os = "windows") {
+        // TODO: add windows support
+    } else if cfg!(target_os = "macos") && which("open").is_ok() {
+        return Ok("open".to_string());
+    } else if cfg!(target_os = "linux") {
+        let candidates = vec!["xdg-open", "gnome-open", "kde-open"];
+        for candidate in candidates {
+            if which(candidate).is_ok() {
+                return Ok(candidate.to_string());
+            }
+        }
+    }
+    Err("No opener found".into())
 }
 
 fn main() {
