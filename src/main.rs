24ba12e5fde773d23605f6046a036f5ebd6647c4
Shinya Fujino <shf0811@gmail.com>
2023-06-24 16:38:13 +0900

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// xop opens files from a shell
+pub struct Args {
+    /// Files to open
+    #[arg(value_name = "FILES")]
+    files: Vec<String>,
+}
+
+pub fn run(args: Args) -> MyResult<()> {
+    println!("{:?}", args);
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
